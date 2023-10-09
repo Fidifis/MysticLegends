2289597a1c881664f89913b3a4df23cf0ee31649
Fidifis <fidifis@seznam.cz>
2023-10-09 22:58:43 +0200

    Current state

---
+++ MysticLegendsClient/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MysticLegendsClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] SplashImages = { "/images/Graphics/LoginScreen.png", "/images/Graphics/LoginScreen2.png", "/images/Graphics/LoginScreen3.png" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChangeSplashImage(ChooseRandom(SplashImages));
+        }
+
+        private static string ChooseRandom(string[] list)
+        {
+            Random random = new();
+            int index = random.Next(list.Length);
+            return list[index];
+        }
+
+        private void ChangeSplashImage(string resource)
+        {
+            BitmapImage bitmapImage = new BitmapImage(new Uri($"pack://application:,,,{resource}", UriKind.Absolute));
+            splashImage.Source = bitmapImage;
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            new CityWindow().Show();
+            Close();
+        }
+
+        private void ServerSelect_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (customServerControls is null) return;
+            customServerControls.Visibility =
+                serverSelect.SelectedIndex == serverSelect.Items.Count - 1 ?
+                Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
