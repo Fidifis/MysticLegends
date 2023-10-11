bf3203cd96c5d27459dce66f0453dd62fdc5180b
Fidifis <fidifis@seznam.cz>
2023-10-11 21:17:05 +0200

    Add icons

--- MysticLegendsClient/CityWindows/AyreimCity.cs
+++ MysticLegendsClient/CityWindows/AyreimCity.cs
@@ -7,7 +7,8 @@ namespace MysticLegendsClient.CityWindows
     {
         public AyreimCity(): base("Ayreim")
         {
-            CityModulesPanel.Children.Add(new CityModuleButton { InnerPadding="20 10 20 10", Margin=new Thickness(0, 0, 0, 10), TextGap=50, FontSize=20, UniformSvgSize="50", SvgSource="/icons/anvil.svg", LabelText="Dark Alley" });
+            CityModulesPanel.Children.Add(new CityModuleButton { InnerPadding="20 10 20 10", Margin=new Thickness(0, 0, 0, 10), TextGap=50, FontSize=20, UniformSvgSize="50", SvgSource="/icons/dragon-solid.svg", LabelText="Scout" });
+            CityModulesPanel.Children.Add(new CityModuleButton { InnerPadding="20 10 20 10", Margin=new Thickness(0, 0, 0, 10), TextGap=50, FontSize=20, UniformSvgSize="50", SvgSource="/icons/rebel.svg", LabelText="Dark Alley" });
         }
     }
 }
