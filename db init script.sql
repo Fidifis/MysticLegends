cc5ae10726464b7126f10e7fc2d4b8d7a0803381
Filip Digrín <fdigrin@thetrask.com>
2023-11-08 11:16:41 +0100

    Update init script

--- db init script.sql
+++ db init script.sql
@@ -1,5 +1,5 @@
--- Insert data into the "user" table
-INSERT INTO "user" (username, password_hash) VALUES ('zmrdus', 'your_password_hash');
+-- Insert data into the "users" table
+INSERT INTO "users" (username, password_hash) VALUES ('zmrdus', 'your_password_hash');
 
 -- Insert data into the "city" table
 INSERT INTO city (city_name) VALUES ('Ayreim');
@@ -33,19 +33,19 @@ VALUES ('zmrdus', 10);
 INSERT INTO npc (npc_id, city_name, npc_type, currency_gold)
 VALUES (1, 'Ayreim', 1, 500);
 
-INSERT INTO npc_item (npc_item_id, npc_id, price_gold)
-VALUES (1, 1, 100),
-       (2, 1, 200),
-       (3, 1, 10);
-
 -- Insert data into the "inventory_item" table
-INSERT INTO inventory_item (invitem_id, character_inventory_character_n, character_name, npc_item_id, npc_id, item_id, level, stack_count, position)
-VALUES (1, 'zmrdus', NULL, NULL, NULL, 1, 1, 1, 0),
-       (2, 'zmrdus', NULL, NULL, NULL, 2, 1, 1, 1),
-       (3, 'zmrdus', NULL, NULL, NULL, 1, 1, 1, 2),
-       (4, NULL, NULL, 1, 1, 1, 1, 1, 3),
-       (5, NULL, NULL, 2, 1, 2, 1, 1, 4),
-       (6, NULL, NULL, 3, 1, 3, NULL, 1, 5);
+INSERT INTO inventory_item (invitem_id, character_inventory_character_n, character_name, npc_id, item_id, level, stack_count, position)
+VALUES (1, 'zmrdus', NULL, NULL, 1, 1, 1, 0),
+       (2, 'zmrdus', NULL, NULL, 2, 1, 1, 1),
+       (3, 'zmrdus', NULL, NULL, 1, 1, 1, 2),
+       (4, NULL, NULL, 1, 1, 1, 1, 3),
+       (5, NULL, NULL, 1, 2, 1, 1, 4),
+       (6, NULL, NULL, 1, 3, NULL, 1, 5);
+	   
+INSERT INTO price (invitem_id, price_gold)
+VALUES (4, 200),
+	(5, 200),
+	(6, 200);
 
 -- Insert data into the "quest" table
 INSERT INTO quest (quest_id, npc_id, name, description, is_repeable, is_offered)
@@ -67,7 +67,6 @@ VALUES (1, 0, 1, 50),  -- Example stat 1
 SELECT setval('inventory_item_invitem_id_seq', (SELECT max(invitem_id) FROM inventory_item));
 SELECT setval('item_item_id_seq', (SELECT max(item_id) FROM item));
 SELECT setval('mob_mob_id_seq', (SELECT max(mob_id) FROM mob));
-SELECT setval('npc_item_npc_item_id_seq', (SELECT max(npc_item_id) FROM npc_item));
 SELECT setval('npc_npc_id_seq', (SELECT max(npc_id) FROM npc));
 SELECT setval('quest_quest_id_seq', (SELECT max(quest_id) FROM quest));
 SELECT setval('refresh_token_record_id_seq', (SELECT max(record_id) FROM refresh_token));
